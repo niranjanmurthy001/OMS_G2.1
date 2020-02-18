453c72d75fe08960877070cb9b9f3a510d29c51d
Niranjan <niranjanmruthy@drnds.com>
2020-02-18 22:39:01 +0530

    Added Keywords form for Vendors - Tukaram

---
+++ Ordermanagement_01/Vendors/Keywords.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Ordermanagement_01.Vendors
+{
+    partial class Keywords
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Keywords
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 324);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Glow;
+            this.Name = "Keywords";
+            this.Text = "Keywords";
+            this.Load += new System.EventHandler(this.Keywords_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ordermanagement_01/Vendors/Keywords.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraEditors;
+using System;
+
+namespace Ordermanagement_01.Vendors
+{
+    public partial class Keywords : XtraForm
+    {
+        public Keywords()
+        {
+            InitializeComponent();
+        }
+        private void Keywords_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

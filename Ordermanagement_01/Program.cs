0b8d163d219237c1e45550497db69b7dfd8f70de
Niranjan <niranjanmruthy@drnds.com>
2020-02-22 00:42:48 +0530

    OrderEntry Binding lookUpEdits - Tukaram

--- Ordermanagement_01/Program.cs
+++ Ordermanagement_01/Program.cs
@@ -14,11 +14,11 @@ namespace Ordermanagement_01
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-             Application.Run(new New_Dashboard.NewLogin());
+            Application.Run(new New_Dashboard.Orders.OrderEntry());
             //Application.Run(new New_Dashboard.Settings.Process_Settings());
             //Application.Run(new New_Dashboard.Settings.EmailSetting());
-           // Application.Run(new Vendors.Keywords(1));
-           
+            // Application.Run(new Vendors.Keywords(1));
+
         }
     }
 }

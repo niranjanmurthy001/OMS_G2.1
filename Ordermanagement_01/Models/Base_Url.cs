2909d19f5782357c6b486e1ec282c5a538530ef4
Niranjan <niranjanmruthy@drnds.com>
2020-02-21 19:12:52 +0530

    Changed

--- Ordermanagement_01/Models/Base_Url.cs
+++ Ordermanagement_01/Models/Base_Url.cs
@@ -6,14 +6,16 @@
         private static readonly string Token_URI;
         static Base_Url()
         {
-            Token_URI = "http://localhost:28537";
-            URI = "http://localhost:28537/Api";
 
-            // URI = "https://titlelogy.com/title_Production_Api_demo/Api";
 
-            //URI = "https://titlelogy.com/TestApi";
+            //Token_URI = "http://localhost:28537";
+            //URI = "http://localhost:28537/Api";
 
-          
+            URI = "https://titlelogy.com/title_Production_Api_demo/Api";
+
+            Token_URI = "https://titlelogy.com/title_Production_Api_demo";
+
+
         }
         public static string Url => URI;
         public static string Token_Url => Token_URI;

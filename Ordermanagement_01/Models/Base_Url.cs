43243adf520bdc1baa56e87ce7c9d8d31567fbe7
Niranjan <niranjanmruthy@drnds.com>
2020-02-21 18:25:12 +0530

    Updated Token Authentication - Niranjan

--- Ordermanagement_01/Models/Base_Url.cs
+++ Ordermanagement_01/Models/Base_Url.cs
@@ -3,14 +3,19 @@
     static class Base_Url
     {
         private static readonly string URI;
+        private static readonly string Token_URI;
         static Base_Url()
         {
-           // URI = "http://localhost:28537/Api";
+            Token_URI = "http://localhost:28537";
+            URI = "http://localhost:28537/Api";
 
-            URI = "https://titlelogy.com/title_Production_Api_demo/Api";
+            // URI = "https://titlelogy.com/title_Production_Api_demo/Api";
 
             //URI = "https://titlelogy.com/TestApi";
+
+          
         }
         public static string Url => URI;
+        public static string Token_Url => Token_URI;
     }
 }
